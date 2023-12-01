332daebb4c024887c5e16b65de6e36a4a06cfde4
Ryan Frazier <rfrazier716@gmail.com>
2023-11-30 23:50:32 -0500

    initial commit. Set up Templates for Rust. GLHF!

---
+++ README.md
@@ -0,0 +1,18 @@
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/rfrazier716/advent-of-code-2023">
+    <img src="images/hogfather.png" alt="Logo" width="200" height="200">
+  </a>
+
+  <h2 align="center">Advent of Code 2023</h2>
+
+  <p align="center">
+    Solutions to 2023's <a href="https://adventofcode.com">Advent of Code</a> challenges, written in Rust... and whichever other language I feel like exploring.
+  </p>
+  <p align="center">
+ ★★★★★ ★★★★★ ★★★★★ ★★★★★ ★★★★★
+  </p>
+</div> 
+
+<!--⭐ -->
---
+++ plopfile.mjs
@@ -0,0 +1,61 @@
+import dotenv from "dotenv";
+import path from "path";
+
+const getInput = async ({ year, day, session }) => {
+  const resp = await fetch(
+    `https://www.adventofcode.com/${year}/day/${day.padStart(2, "0")}/input`,
+    {
+      method: "GET",
+      headers: { session: session },
+    }
+  );
+  return resp.text();
+};
+
+export default function (plop) {
+  plop.setGenerator("rust", {
+    description: "Create a new day's Puzzle for Rust",
+    prompts: [
+      {
+        type: "input",
+        name: "day",
+        message: "What Day of the puzzle to initialize?",
+      },
+    ],
+
+    actions: (data) => {
+      data.day = data.day.padStart(2, "0");
+      const actions = [];
+      const env = dotenv.config({
+        path: path.join(plop.getPlopfilePath(), ".env"),
+      }).parsed;
+      const input = getInput({ ...data, ...env });
+      actions.push(
+        async (data) => {
+          data.input = await getInput({ ...data, ...env });
+          return "Downloaded Puzzle Input!";
+        },
+        {
+          type: "addMany",
+          destination: "rust/day_{{day}}",
+          base: `templates/rust`,
+          templateFiles: `templates/**/*.hbs`,
+        },
+        {
+          type: "add",
+          path: "rust/day_{{day}}/input.txt",
+          template: "{{input}}",
+        },
+        {
+          type: "append",
+          path: "Cargo.toml",
+          pattern: "#plop-members-prefix",
+          template: '\t"rust/day_{{day}}",',
+        },
+        () =>
+          `Find Today's Puzzle at https://adventofcode.com/${env.year}/day/${data.day}`
+      );
+      return actions;
+    },
+  });
+}
